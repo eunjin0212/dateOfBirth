ec5b6aee37e2fc0da426d0d9ae322aaf40a121d4
eunjin0212 <eunjin0212@gmail.com>
2023-05-09 22:27:46 +0900

    📈 google-site-verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="UaXwgG63CIzBBZoaC_wokIcGUxfqUeBIaCgqjH3TTA0" />
         <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2816486920024458" crossOrigin="anonymous" />
       </Head>
       <body>
