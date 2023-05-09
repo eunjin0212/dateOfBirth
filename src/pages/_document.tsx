4e91632b9a786af5493705f05323aad239cc61b6
eunjin0212 <eunjin0212@gmail.com>
2023-05-09 21:35:21 +0900

    🩹 구글 스크립트 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2816486920024458" crossOrigin="anonymous" />
+      </Head>
       <body>
         <Main />
         <NextScript />
